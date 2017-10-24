1e28ccf84df67eb50e4b2ab39b7574d4820f0817
Jason Chafin <jason@blackbirdconsult.com>
2017-10-24 09:23:00 -0700

    fixing responsive menu

--- lib/functions/load-assets.php
+++ lib/functions/load-assets.php
@@ -24,13 +24,13 @@ function enqueue_assets() {
 
 	wp_enqueue_style( CHILD_TEXT_DOMAIN .'-fonts', '//fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700', array(), CHILD_THEME_VERSION );
 	wp_enqueue_style( 'dashicons' );
-	wp_enqueue_script( CHILD_TEXT_DOMAIN .'-back-to-top', CHILD_URL . '/js/blackbird-back-to-top.js', array( 'jquery' ), CHILD_THEME_VERSION, true );
+	wp_enqueue_script( CHILD_TEXT_DOMAIN .'-back-to-top', CHILD_URL . '/assets/js/blackbird-back-to-top.js', array( 'jquery' ), CHILD_THEME_VERSION, true );
 
 	// FontAwesome
     wp_enqueue_script( 'font-awesome', 'https://use.fontawesome.com/64d0d03cae.js',  null, true);
     
 
-	wp_enqueue_script( CHILD_TEXT_DOMAIN .'-responsive-menu', CHILD_URL . '/js/responsive-menu.js', array( 'jquery' ), CHILD_THEME_VERSION, true );
+	wp_enqueue_script( CHILD_TEXT_DOMAIN .'-responsive-menu', CHILD_URL . '/assets/js/responsive-menu.js', array( 'jquery' ), CHILD_THEME_VERSION, true );
 	$localized_script_args = array(
 		'mainMenu' => __( 'Menu', CHILD_TEXT_DOMAIN ),
 		'subMenu'  => __( 'Menu', CHILD_TEXT_DOMAIN ),
